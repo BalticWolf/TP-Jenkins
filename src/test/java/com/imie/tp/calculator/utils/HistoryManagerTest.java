880386173a01d877367815981352c893a16758d7
Timothee Aupetit <timothee.aupetit@compte-nickel.fr>
2018-01-26 13:34:09 +0100

    added test getHistory

--- src/test/java/com/imie/tp/calculator/utils/HistoryManagerTest.java
+++ src/test/java/com/imie/tp/calculator/utils/HistoryManagerTest.java
@@ -21,8 +21,15 @@ public class HistoryManagerTest {
 
     @Test
     public void addOperation() {
+        assertNotNull(this.historyManager);
         this.historyManager.addOperation("0 + 0 = 0");
         this.historyManager.addOperation("1 + 2 = 3");
         assertEquals(2, this.historyManager.getLatestOperations().size());
+    }
+
+    @Test
+    public void getLatestOperations() {
+        assertNotNull(this.historyManager);
+        assertNotNull(this.historyManager.getLatestOperations());
     }
 }
