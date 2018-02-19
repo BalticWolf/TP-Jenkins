9d39ea3ff4629b225fad99d556ccef72d4c9dc62
Timothee Aupetit <timothee.aupetit@compte-nickel.fr>
2018-02-19 17:19:11 +0100

    Refactored Application class

--- src/main/java/com/imie/tp/calculator/Application.java
+++ src/main/java/com/imie/tp/calculator/Application.java
@@ -1,6 +1,9 @@
 package com.imie.tp.calculator;
 
 import com.imie.tp.calculator.operation.AdditionOperation;
+import com.imie.tp.calculator.operation.DivisionOperation;
+import com.imie.tp.calculator.operation.MultiplicationOperation;
+import com.imie.tp.calculator.operation.SubtractionOperation;
 import com.imie.tp.calculator.utils.HistoryManager;
 import com.imie.tp.calculator.utils.KeyboardUtils;
 
@@ -13,7 +16,7 @@ public final class Application {
      * Default constructor.
      */
     private Application() {
-        // cannot be call from children
+        // cannot be called from children
     }
 
     /**
@@ -23,33 +26,24 @@ public final class Application {
     public static void main(final String[] args) {
 
         final HistoryManager historyManager = new HistoryManager();
-        String operation; // string representation of an operation
         int input; // store input from keyboard
-        float operandA;
-        float operandB;
-        float result;
+
+        String operation = "";
+
         do {
             input = Integer.parseInt(KeyboardUtils.readFromKeyboard("Type of Operation: "));
             switch (input) {
             case 1:
-                operandA = Float.parseFloat(KeyboardUtils.readFromKeyboard("Enter Value a: "));
-                final AdditionOperation addition = new AdditionOperation(operandA);
-
-                operandB = Float.parseFloat(KeyboardUtils.readFromKeyboard("Enter Value b: "));
-                result = addition.make(operandB);
-
-                operation = operandA + " + " + operandB + " = " + result;
-                System.out.println(operation);
-                historyManager.addOperation(operation);
+                operation = handleAddition(promptOperand("a"), promptOperand("b"));
                 break;
             case 2:
-                System.out.println("subtraction");
+                operation = handleSubtraction(promptOperand("a"), promptOperand("b"));
                 break;
             case 3:
-                System.out.println("division");
+                operation = handleDivision(promptOperand("a"), promptOperand("b"));
                 break;
             case 4:
-                System.out.println("multiplication");
+                operation = handleMultiplication(promptOperand("a"), promptOperand("b"));
                 break;
             case 5:
                 historyManager.showHistory();
@@ -58,6 +52,43 @@ public final class Application {
                 System.out.println("Wrong item: enter 1 to 5 for operation, or 9 to exit");
                 break;
             }
+            if (operation.length() > 0) {
+                System.out.println(operation);
+                historyManager.addOperation(operation);
+            }
+            operation = "";
         } while (input != 9);
+    }
+
+    private static String handleAddition(final float operandA, final float operandB) {
+
+        final AdditionOperation addition = new AdditionOperation(operandA);
+
+        return operandA + " + " + operandB + " = " + addition.make(operandB);
+    }
+
+    private static String handleSubtraction(final float operandA, final float operandB) {
+
+        final SubtractionOperation sub = new SubtractionOperation(operandA);
+
+        return operandA + " - " + operandB + " = " + sub.make(operandB);
+    }
+
+    private static String handleMultiplication(final float operandA, final float operandB) {
+
+        final MultiplicationOperation multiplication = new MultiplicationOperation(operandA);
+
+        return operandA + " x " + operandB + " = " + multiplication.make(operandB);
+    }
+
+    private static String handleDivision(final float operandA, final float operandB) {
+
+        final DivisionOperation multiplication = new DivisionOperation(operandA);
+
+        return operandA + " / " + operandB + " = " + multiplication.make(operandB);
+    }
+
+    private static float promptOperand(String member) {
+        return Float.parseFloat(KeyboardUtils.readFromKeyboard("Enter Value " + member + ": "));
     }
 }
